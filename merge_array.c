94c9b5ca3f9b5f6e053a6068597cdc1f7c72c54c
Nesmin Hashim <nesminn.hashim@gmail.com>
2026-08-10 18:03:41 +0530

    Implement merging of two sorted arrays
    This program merges two sorted arrays into a single sorted array. It prompts the user for the sizes and elements of the two arrays, then combines them and displays the merged result.

---
+++ merge_array.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr1[100], arr2[100], arr3[200];
+    int n1, n2;
+    int i = 0, j = 0, k = 0;
+
+    printf("Enter the size of first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter the elements of first sorted array:\n");
+
+    for (i = 0; i < n1; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter the size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter the elements of second sorted array:\n");
+
+    for (j = 0; j < n2; j++)
+    {
+        scanf("%d", &arr2[j]);
+    }
+
+    /* Compare and merge the two arrays */
+    i = 0;
+    j = 0;
+
+    while (i < n1 && j < n2)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            arr3[k] = arr1[i];
+            i++;
+        }
+        else
+        {
+            arr3[k] = arr2[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    /* Add remaining elements of first array */
+    while (i < n1)
+    {
+        arr3[k] = arr1[i];
+        i++;
+        k++;
+    }
+
+    /* Add remaining elements of second array */
+    while (j < n2)
+    {
+        arr3[k] = arr2[j];
+        j++;
+        k++;
+    }
+
+    /* Display the merged array */
+    printf("Merged sorted array:\n");
+
+    for (i = 0; i < n1 + n2; i++)
+    {
+        printf("%d ", arr3[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
